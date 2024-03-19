67558f8f1f0e064373699bdbf4fd621ecc689d4b
InimitablePioneer <seungwonpioneer@gmail.com>
2024-03-19 16:33:36 +0900

    Mar19 at work via WIN

---
+++ src/main/java/hello/itemservice/domain/item/Testing.java
@@ -0,0 +1,8 @@
+package hello.itemservice.domain.item;
+
+public class Testing {
+    public static void main(String[] args) {
+        //ItemType.BOOK.description = "arar";
+        //System.out.println(ItemType.BOOK.description);
+    }
+}
